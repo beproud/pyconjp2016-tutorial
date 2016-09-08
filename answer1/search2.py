e07dc7625baee8c7d318ce4ab876234a260b8386
crohaco <crohaco@beproud.jp>
2016-09-08 21:23:49 +0900

    回答例の追加

---
+++ answer1/search2.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+import os
+
+
+def search(target_path, keyword):
+    if not os.path.isdir(target_path):
+        print('パスにはディレクトリを指定してください')
+        return
+
+    for filename in os.listdir(target_path):
+        filepath = os.path.join(target_path, filename)
+
+        if not os.path.isdir(filepath):
+            with open(filepath, encoding='utf-8') as f:
+                if keyword in f.read():
+                    print(filename)
+
+
+if __name__ == '__main__':
+    search('notes', '雪')
---
+++ answer1/search5.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+import os
+import re
+
+NUM_CHARS = 5
+
+
+def search(target_path, keyword):
+    if not os.path.exists(target_path):
+        print('指定したパスが存在しません')
+        return
+
+    if not os.path.isdir(target_path):
+        print('パスにはディレクトリを指定してください')
+        return
+
+    for path, dirnames, filenames in os.walk(target_path):
+        for filename in filenames:
+            filepath = os.path.join(path, filename)
+
+            if not os.path.isdir(filepath):
+                with open(filepath, encoding='utf-8') as f:
+                    text = f.read().replace('\n', '')
+                    match = re.search(keyword, text)
+                    if match:
+                        print('{filepath}: {prefix}\033[92m{main}\033[0m{suffix}'.format(
+                            filepath=filepath,
+                            prefix=text[match.start() - NUM_CHARS:match.start()],
+                            main=text[match.start():match.end()],
+                            suffix=text[match.end():match.end() + NUM_CHARS]
+                        ))
+
+
+if __name__ == '__main__':
+    search('notes', '雪')
